fbb26e7fd8a83fcd284677e5eece899895f3eb81
NicolasCardenasL <nicolascardenas0115@gmail.com>
2025-07-18 10:23:54 -0500

    created the webhook for user handleling

---
+++ convex/clerkWebhook.ts
@@ -0,0 +1,35 @@
+"use node";
+import type { WebhookEvent } from "@clerk/clerk-sdk-node";
+import { v } from "convex/values";
+import { Webhook } from "svix";
+import { internalAction } from "./_generated/server";
+
+/* 
+Internal action created in order to access the node enviorenment
+This is due to a usage of the library svix
+*/
+
+/* 
+Grabs the secret token to verify the webhook from your .env file.
+*/
+const WEB_HOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET as string;
+export const fulfill = internalAction({
+  args: {
+    headers: v.any(),
+    payload: v.string(),
+  },
+  handler: async (ctx, args) => {
+    /*
+    Svix verifies the payload signature to ensure it's not forged    
+    Verifica la firma del webhook (asegura que realmente vino de Clerk o quien sea que esté enviando el webhook).
+    Lanza un error si la firma no es válida.
+    Devuelve el cuerpo del webhook ya parseado como objeto si todo está bien.
+    */
+    const wh = new Webhook(WEB_HOOK_SECRET);
+    const payload = wh.verify(args.payload, args.headers) as WebhookEvent;
+    /* 
+    Returns a valid payload (action response) from clerk: { "id": "123", "event": "user.created" }
+    */
+    return payload;
+  },
+});
---
+++ convex/http.ts
@@ -0,0 +1,68 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { internal } from "./_generated/api";
+
+const http = httpRouter();
+
+/*
+  This creates an http action (a public function accesible by an http request) 
+  Every time clerk handles a new user it access our convex storage through this end-point
+  and updates de user information storaged in our database
+*/
+http.route({
+  path: "/clerk",
+  method: "POST",
+  handler: httpAction(async (ctx, req) => {
+    const payloadString = await req.text();
+    const headerPayload = req.headers;
+
+    try {
+      /* Access and verifies the clerk response */
+      const result = await ctx.runAction(internal.clerkWebhook.fulfill, {
+        payload: payloadString,
+        headers: {
+          "svix-id": headerPayload.get("svix-id")!,
+          "svix-signature": headerPayload.get("svix-signature")!,
+          "svix-timestamp": headerPayload.get("svix-timestamp")!,
+        },
+      });
+
+      /* 
+        Updated the user storage in convex based on the action type 
+        handled by Clerk
+      */
+      switch (result.type) {
+        case "user.created":
+          await ctx.runMutation(internal.users.createUser, {
+            tokenIdentifier: `${process.env.CLERK_APP_DOMAIN}|${result.data.id}`,
+            fullName: `${result.data.first_name} ${result.data.last_name}`,
+            profileImg: result.data.image_url,
+          });
+          break;
+        case "user.updated":
+          await ctx.runMutation(internal.users.updateUser, {
+            tokenIdentifier: `${process.env.CLERK_APP_DOMAIN}|${result.data.id}`,
+            profileImg: result.data.image_url,
+          });
+          break;
+        case "user.deleted":
+          await ctx.runMutation(internal.users.deleteUser, {
+            tokenIdentifier: `${process.env.CLERK_APP_DOMAIN}|${result.data.id}`,
+          });
+          break;
+      }
+
+      /* Return a successful connection response */
+      return new Response(null, {
+        status: 200,
+      });
+    } catch (error) {
+      console.log("Webhook Error", error);
+      return new Response("Webhook Error", {
+        status: 400,
+      });
+    }
+  }),
+});
+
+export default http;
