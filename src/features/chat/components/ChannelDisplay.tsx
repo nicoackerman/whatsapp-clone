d325db9c2d7c81c5313888ac73a6949b2512b8d9
nicolas <nicolascardenas0115@gmail.com>
2025-08-27 15:01:47 +0000

    fix the background image of each chat

--- src/features/chat/components/ChannelDisplay.tsx
+++ src/features/chat/components/ChannelDisplay.tsx
@@ -6,14 +6,13 @@ import ChannelHeader from "./ChannelHeader";
 import ChannelInput from "./ChannelInput";
 import type { ChannelIdentifier } from "~/types";
 
-
 export default function ChannelDisplay() {
   const currentChannelIdentifier: ChannelIdentifier | "placeholder" =
     useChatStore((state) => state.currentChannelIdentifier);
   return (
     <div
       key={currentChannelIdentifier}
-      className="flex h-screen w-full flex-col bg-[url('/bg-light.png')] dark:bg-[url('/bg-dark.png')]"
+      className="flex h-screen w-full flex-col bg-[url('/bg-light.png')] bg-cover bg-center dark:bg-[url('/bg-dark.png')]"
     >
       <ChannelHeader className="border border-gray-700/10 bg-white p-4 dark:bg-[#161717]" />
       <ChannelContent />
